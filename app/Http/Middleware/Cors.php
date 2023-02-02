aa35e2c4c53bbb65b743f1762168f74c9ac561be
s1gmas1x <sigma_vi@yahoo.com>
2023-02-02 13:21:28 -0700

    getting cors set up

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->header('Access-Control-Allow-Origin', 'https://ckohl.com')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUSH, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-REQUESTED-WITH, x-xsrf-token')
             ->header('Access-Control-Allow-Credentials', 'true')
